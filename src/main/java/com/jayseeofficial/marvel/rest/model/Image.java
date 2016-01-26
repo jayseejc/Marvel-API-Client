5abc3c2004d92d4181e175711696f79ca404f49c
Jon Champagne <jayseejc3191@gmail.com>
2016-01-26 11:40:03 -0500

    Add constants that can be used for an image when there isn't one avalable.

--- src/main/java/com/jayseeofficial/marvel/rest/model/Image.java
+++ src/main/java/com/jayseeofficial/marvel/rest/model/Image.java
@@ -5,6 +5,10 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import java.io.Serializable;
 
 public class Image implements Serializable {
+
+    public static final String NOT_FOUND_PATH="https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+    public static final String NOT_FOUND_EXTENTION="jpg";
+
     private String path;
     private String extension;
 
